381252377ad829b2b7fe44906c1f556fb9255887
bholu singh <bholus079@outlook.com>
2025-07-18 09:16:53 +0530

    Updated files

---
+++ try_ch5/5.3.py
@@ -0,0 +1,26 @@
+#solution of ex 5.3
+
+#assigning  color green and checking if it is true
+alien_color = "green"
+if alien_color == "green":
+    print("you have earned 5 points")
+
+    
+#assigning new color yellow and checking if it is true
+alien_color = "yellow"
+if alien_color == "green": 
+    print("you have earned 5 points")
+
+#assigning new color red and checking if it is true
+alien_color = "red"
+if alien_color == "green":
+    print("you have earned 5 points")
+
+#second question of this exercise
+alien_color = "green"
+if alien_color =="green":
+    print(f"alien_color is {alien_color}")
+
+if alien_color == "yellow": #condition failed no output
+    print(f"alien_color is {alien_color}")
+    
