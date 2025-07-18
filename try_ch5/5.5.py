8b4b509d5a4f52846d6c6d0611ee3a8797ba3426
bholu singh <bholus079@outlook.com>
2025-07-18 14:12:11 +0530

    Updated files

---
+++ try_ch5/5.5.py
@@ -0,0 +1,34 @@
+#creating three versions of program
+#solution exersice 5.5
+
+alien_color = "green" #set alien_color to green
+if (alien_color =="green"):
+    print("you have earned 5 points!")
+
+elif (alien_color == "yellow"):
+    print("you have earned 10 points!")
+
+else:
+    print("you have earned 15 points!")
+
+#second version
+alien_color = "yellow"  #set alien color to yellow
+if (alien_color =="green"):
+    print("you have earned 5 points!")
+    
+elif (alien_color == "yellow"):
+    print("you have earned 10 points!")
+
+else:
+    print("you have earned 15 points!")
+
+#third version
+alien_color = "red"  #set alien color to red
+if (alien_color =="green"):
+    print("you have earned 5 points!")
+
+elif (alien_color == "yellow"):
+    print("you have earned 10 points!")
+
+else:
+    print("you have earned 15 points!")
