3f4a7665156fee510217e72fb2c10ef9c0679c65
bholu singh <bholus079@outlook.com>
2025-07-18 15:22:23 +0530

    Updated files

---
+++ try_ch5/5.7.py
@@ -0,0 +1,25 @@
+# solution of exercise 5.6
+#list of my favorite_fruits
+favorite_fruits = ["banana","mango","apple","grapes","coconut"]
+
+#five checks if item in list
+if "mango" in favorite_fruits:
+    print("you really likes mango")
+
+if "banana" in favorite_fruits:
+    print("you really like banana")
+
+if "apple" in favorite_fruits:
+    print("you really like apple")
+
+if "coconut" in favorite_fruits:
+    print("you really like coconut")
+
+if "grapes" in favorite_fruits:
+    print("you really like grapes")
+
+
+#if you want to use loop    
+#for i in favorite_fruits:
+#    if i in favorite_fruits:
+#        print(f"you really likes {i}")
