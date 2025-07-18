7a742405eee90bb5b3ed3f68347faf39fde187e1
bholu singh <bholus079@outlook.com>
2025-07-18 13:18:17 +0530

    Updated files

---
+++ try_ch5/5.4.py
@@ -0,0 +1,18 @@
+#solution of 5.4 ex
+alien_color = "green"  #set alien_color to green
+if (alien_color =="green"):
+    print("you have earned 5 points!")
+
+else:
+    print("you have just earned 10 points!")
+
+#another version of program (alien_color != "green")
+
+alien_color = "yellow"  #set alien_color to yellow
+
+if(alien_color=="green"):
+    print("you have earned just 5 points")
+    
+else:
+    print("you got 10 points")
+
