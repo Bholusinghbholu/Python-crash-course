d730a0f03747a0f06464f73612cc80ab8d530ab0
bholu singh <bholus079@outlook.com>
2025-07-20 11:04:25 +0530

    Updated files

---
+++ try_ch5/5.8.py
@@ -0,0 +1,11 @@
+#solution exercise 5.8
+#usernames names list
+usernames = ["nikhil","nishant","admin","ayush","golu"]
+
+for username in usernames:      #looping through list
+
+    if username == "admin":      #special message for admin
+        print(f"Hello {username}, would you like to see a ststus report?")
+    else:
+        print(f"Hello {username}, thank you for login again.")
+        
