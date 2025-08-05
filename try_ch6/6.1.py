4273fb2fba936d0a20b92bdf03411a2444e4c24f
bholu singh <bholus079@outlook.com>
2025-08-05 11:53:15 +0530

    Update

---
+++ try_ch6/6.1.py
@@ -0,0 +1,5 @@
+person = {"first_name":"bholu", "last_name":"Singh", "city":"kanpur", "age":1}
+print(f"first_name :{person["first_name"]}")
+print(f"last_name :{person["last_name"]}")
+print(f"city :{person["city"]}")
+print(f"age :{person["age"]}")
---
+++ try_ch6/6.2.py
@@ -0,0 +1,6 @@
+favorite_numbers ={"ganesh":1,"raman":3,"sumit":5,"bholu":19,"golu":7}
+print(f"favorite_number of ganesh is {favorite_numbers['ganesh']} ")
+print(f"favorite_number of golu is {favorite_numbers['golu']} ")
+print(f"favorite_number of raman is {favorite_numbers['raman']} ")
+print(f"favorite_number of bholu is {favorite_numbers['bholu']} ")
+print(f"favorite_number of sumit is {favorite_numbers['sumit']} ")
