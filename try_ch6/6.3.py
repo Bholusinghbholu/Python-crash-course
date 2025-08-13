6887c9d2f83ec63652508f2eee34b7bc33d3f2e2
bholu singh <bholus079@outlook.com>
2025-08-13 13:29:33 +0530

    Updated files

---
+++ try_ch6/6.3.py
@@ -0,0 +1,18 @@
+glossary = {
+"print":"display output",
+"int":"integer data types",
+"str":"string data types",
+"list":"store data of diffrent types",
+"dictonry":"work like real dictonry",
+}
+
+print(f"print :{glossary["print"]}\n")
+print(f"int :{glossary["int"]}\n")
+print(f"str :{glossary["str"]}\n")
+print(f"list :{glossary["list"]}\n")
+print(f"dictonry :{glossary["dictonry"]}\n")
+
+
+
+
+
