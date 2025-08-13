ce194c9ba41d746fb31ea5cc3ce5f0d1bdc28686
bholu singh <bholus079@outlook.com>
2025-08-13 14:20:26 +0530

    Updated files

---
+++ try_ch6/6.4.py
@@ -0,0 +1,15 @@
+glossary = {
+"print":"display output",
+"int":"integer data types",
+"str":"string data types",
+"list":"store data of diffrent types",
+"dictonry":"work like real dictonry",
+"loop":"do repeted task",
+"if":"condition",
+"tuple":"like list but we cant change values",
+"set":"store unique data",
+"sort":"sort data",
+}
+
+for k,v in glossary.items():
+    print(f"{k} :{v}")
