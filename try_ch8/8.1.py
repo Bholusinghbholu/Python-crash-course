a20c9df307d40c3e90cf9bdfc527f90f15f2e15f
bholu singh <bholus079@outlook.com>
2025-09-24 14:25:32 +0530

    Updated files

---
+++ try_ch8/8.1.py
@@ -0,0 +1,5 @@
+def display_message():
+    """aking about learning"""
+    print(f"I am learning functions!")
+
+display_message()
---
+++ try_ch8/8.2.py
@@ -0,0 +1,5 @@
+def favorite_book(title):
+    """disply my favorite book title"""
+    print(f"One of my favorite book is {title.title()}")
+
+favorite_book("harry potter")    
